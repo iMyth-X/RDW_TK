93511f27cf6f70d0dda9a965e4bf3c6e62d214bb
Chris Roda <chris@chrisroda.com>
2017-11-22 13:41:24 -0500

    OK _ I bit bullet and created a new Actor class for the Redirection manager. However, I called it the RDW_Manager_Actor. If we need to transform it again to something else then we will not suck up the name space.

---
+++ Source/RDW_TK/RDW_Manager_Actor.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "RDW_Manager_Actor.h"
+
+
+// Sets default values
+ARDW_Manager_Actor::ARDW_Manager_Actor()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ARDW_Manager_Actor::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ARDW_Manager_Actor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
